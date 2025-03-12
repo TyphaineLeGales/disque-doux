0845ceab7bd81b1f6867e987e1e7994d1da71e96
Typhaine Le Gales <typhaineLeGales@protonmail.com>
2025-03-12 10:57:46 +0100

    feat: zustand user store

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -2,14 +2,17 @@ import { useRef, useEffect } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Rive, { Fit, RiveRef } from 'rive-react-native';
 
+import { useUserStore } from '@/stores/userStore';
+
 export const Timeline = () => {
   const riveComponentRef = useRef<RiveRef>(null);
   //TODO - use persisted zustand state for level completion instead of local ref
-  const levelCompletion = useRef(1);
+
+  const { levelCompleted } = useUserStore();
 
   useEffect(() => {
     if (riveComponentRef.current) {
-      riveComponentRef.current.setInputState('main', 'levelDone', levelCompletion.current);
+      riveComponentRef.current.setInputState('main', 'levelDone', levelCompleted);
     }
   }, [riveComponentRef.current]);
   return (
---
+++ stores/userStore.ts
@@ -0,0 +1,22 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+type State = {
+  levelCompleted: number;
+  isFirstTime: boolean;
+  completeLevel: (qty: number) => void;
+  unsetIsFirstTime: () => void;
+};
+
+export const useUserStore = create<State>()(
+  persist(
+    (set) => ({
+      levelCompleted: 0,
+      isFirstTime: true,
+      completeLevel: (qty: number) =>
+        set((state) => ({ levelCompleted: state.levelCompleted + qty })),
+      unsetIsFirstTime: () => set(() => ({ isFirstTime: false })),
+    }),
+    { name: 'user-storage' }
+  )
+);
